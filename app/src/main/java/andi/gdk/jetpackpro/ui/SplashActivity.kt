39fa6c573f077fd45bf54a82259da723db480e62
andiwijayaid <andiwijayaid@gmail.com>
2019-11-12 01:03:24 +0800

    create splash screen, add movies and tv shows poster, add libraries to gradle

---
+++ app/src/main/java/andi/gdk/jetpackpro/ui/SplashActivity.kt
@@ -0,0 +1,16 @@
+package andi.gdk.jetpackpro.ui
+
+import andi.gdk.jetpackpro.ui.home.HomeActivity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
+
+}
