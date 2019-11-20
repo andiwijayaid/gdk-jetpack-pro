0b047b395528a26202a6c97ac1cb75d9fe8c98ad
andiwijayaid <andiwijayaid@gmail.com>
2019-11-21 02:13:47 +0800

    update MovieDetailActivityTest and TvShowDetailActivityTest with idle resource

--- app/src/androidTest/java/andi/gdk/jetpackpro/ui/home/detail/MovieDetailActivityTest.kt
+++ app/src/androidTest/java/andi/gdk/jetpackpro/ui/home/detail/MovieDetailActivityTest.kt
@@ -1,15 +1,29 @@
 package andi.gdk.jetpackpro.ui.home.detail
 
+import andi.gdk.jetpackpro.R
+import andi.gdk.jetpackpro.ui.home.movie.MovieFragment.Companion.EXTRA_MOVIE
+import andi.gdk.jetpackpro.ui.home.movie.MovieFragment.Companion.EXTRA_MOVIE_ID
 import andi.gdk.jetpackpro.ui.home.movie.detail.MovieDetailActivity
+import andi.gdk.jetpackpro.utils.EspressoIdlingResource
+import andi.gdk.jetpackpro.utils.convertToCurrency
+import andi.gdk.jetpackpro.utils.generateFakeDummyMovie
+import andi.gdk.jetpackpro.utils.generateFakeDummyMovies
 import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class MovieDetailActivityTest {
 
-//    private var dummyMovie = generateFakeDummyMovies()[0]
+    private var dummyMovie = generateFakeDummyMovies()[0]
+    private var dummyMovieResponse = generateFakeDummyMovie()
 
     @get: Rule
     var activityRule: ActivityTestRule<MovieDetailActivity> = object :
@@ -17,20 +31,37 @@ class MovieDetailActivityTest {
         override fun getActivityIntent(): Intent {
             val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
             val result = Intent(targetContext, MovieDetailActivity::class.java)
-//            result.putExtra(EXTRA_MOVIE_TITLE, dummyMovie.title)
+            result.putExtra(EXTRA_MOVIE, dummyMovie)
+            result.putExtra(EXTRA_MOVIE_ID, dummyMovie.id)
             return result
         }
     }
 
+    @Before
+    fun setUp() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.espressoIdlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.espressoIdlingResource)
+    }
+
     @Test
     fun loadMovie() {
-//        onView(withId(R.id.titleTV)).check(matches(isDisplayed()))
-//        onView(withId(R.id.titleTV)).check(matches(withText(dummyMovie.title)))
-//
-//        onView(withId(R.id.dateTV)).check(matches(isDisplayed()))
-//        onView(withId(R.id.dateTV)).check(matches(withText(dummyMovie.date)))
-//
-//        onView(withId(R.id.overviewTV)).check(matches(isDisplayed()))
-//        onView(withId(R.id.overviewTV)).check(matches(withText("${dummyMovie.overview} \n")))
+        onView(withId(R.id.titleTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.titleTV)).check(matches(withText(dummyMovie.originalTitle)))
+
+        onView(withId(R.id.dateTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.dateTV)).check(matches(withText(dummyMovie.releaseDate)))
+
+        onView(withId(R.id.overviewTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.overviewTV)).check(matches(withText("${dummyMovie.overview} \n")))
+
+        onView(withId(R.id.budgetTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.budgetTV)).check(matches(withText(convertToCurrency(dummyMovieResponse.budget))))
+
+        onView(withId(R.id.revenueTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.revenueTV)).check(matches(withText(convertToCurrency(dummyMovieResponse.revenue))))
     }
 }
--- app/src/androidTest/java/andi/gdk/jetpackpro/ui/home/detail/TvShowDetailActivityTest.kt
+++ app/src/androidTest/java/andi/gdk/jetpackpro/ui/home/detail/TvShowDetailActivityTest.kt
@@ -1,15 +1,28 @@
 package andi.gdk.jetpackpro.ui.home.detail
 
+import andi.gdk.jetpackpro.R
+import andi.gdk.jetpackpro.ui.home.tvshow.TvShowFragment.Companion.EXTRA_TV_SHOW
+import andi.gdk.jetpackpro.ui.home.tvshow.TvShowFragment.Companion.EXTRA_TV_SHOW_ID
 import andi.gdk.jetpackpro.ui.home.tvshow.detail.TvShowDetailActivity
+import andi.gdk.jetpackpro.utils.EspressoIdlingResource
+import andi.gdk.jetpackpro.utils.generateFakeDummyTvShow
+import andi.gdk.jetpackpro.utils.generateFakeDummyTvShows
 import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class TvShowDetailActivityTest {
 
-//    private var dummyTvShow = generateFakeDummyTvShows()[0]
+    private var dummyTvShow = generateFakeDummyTvShows()[0]
+    private var dummyTvShowResponse = generateFakeDummyTvShow()
 
     @get: Rule
     var activityRule: ActivityTestRule<TvShowDetailActivity> = object :
@@ -17,20 +30,37 @@ class TvShowDetailActivityTest {
         override fun getActivityIntent(): Intent {
             val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
             val result = Intent(targetContext, TvShowDetailActivity::class.java)
-//            result.putExtra(EXTRA_TV_SHOW_TITLE, dummyTvShow.title)
+            result.putExtra(EXTRA_TV_SHOW, dummyTvShow)
+            result.putExtra(EXTRA_TV_SHOW_ID, dummyTvShow.id)
             return result
         }
     }
 
+    @Before
+    fun setUp() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.espressoIdlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.espressoIdlingResource)
+    }
+
     @Test
     fun loadTvShow() {
-//        onView(withId(R.id.titleTV)).check(matches(isDisplayed()))
-//        onView(withId(R.id.titleTV)).check(matches(withText(dummyTvShow.title)))
-//
-//        onView(withId(R.id.dateTV)).check(matches(isDisplayed()))
-//        onView(withId(R.id.dateTV)).check(matches(withText(dummyTvShow.date)))
-//
-//        onView(withId(R.id.overviewTV)).check(matches(isDisplayed()))
-//        onView(withId(R.id.overviewTV)).check(matches(withText("${dummyTvShow.overview} \n")))
+        onView(withId(R.id.titleTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.titleTV)).check(matches(withText(dummyTvShow.originalName)))
+
+        onView(withId(R.id.dateTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.dateTV)).check(matches(withText(dummyTvShow.firstAirDate)))
+
+        onView(withId(R.id.overviewTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.overviewTV)).check(matches(withText("${dummyTvShow.overview} \n")))
+
+        onView(withId(R.id.numberOfEpsTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.numberOfEpsTV)).check(matches(withText(dummyTvShowResponse.numberOfEpisodes.toString())))
+
+        onView(withId(R.id.numberOfSeasonTV)).check(matches(isDisplayed()))
+        onView(withId(R.id.numberOfSeasonTV)).check(matches(withText(dummyTvShowResponse.numberOfSeasons.toString())))
     }
 }
